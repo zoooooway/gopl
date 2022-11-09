31d3b2e022e5f6ada8eb65262a3ae33831a2309a
ariohe <70228762+zoooooway@users.noreply.github.com>
2022-11-09 10:56:16 +0800

    lissajous

--- chapter1/dup3/dup3.go
+++ chapter1/dup3/dup3.go
@@ -1,33 +1,32 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func main() {
 	paths := os.Args[1:]
-	counts := make(map[string]int)
-
 	for _, path := range paths {
-		file, err := os.Open(path)
+		counts := make(map[string]int)
+		data, err := ioutil.ReadFile(path)
 		if err != nil {
 			fmt.Fprint(os.Stderr, err.Error())
 			continue
 		}
+		lines := strings.Split(string(data), "\n")
 
-		input := bufio.NewScanner(file)
-		for input.Scan() {
-			line := input.Text()
+		for _, line := range lines {
 			counts[line]++
 		}
-
-	}
-
-	for line, count := range counts {
-		if count > 1 {
-			fmt.Println(count, line)
+		for line, count := range counts {
+			if count > 1 {
+				fmt.Printf("%s:\t", path)
+				fmt.Println(count, line)
+			}
 		}
 	}
+
 }
