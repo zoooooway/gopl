998d32b7b27363829ca54eff20027b60cd0d5017
ariohe <70228762+zoooooway@users.noreply.github.com>
2022-11-09 13:32:52 +0800

    practice

--- chapter1/fetch/main.go
+++ chapter1/fetch/main.go
@@ -5,21 +5,21 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		res, err := http.Get(url)
 		if err != nil {
 			fmt.Fprint(os.Stderr, err)
 			os.Exit(1)
 		}
-		body, err := io.ReadAll(res.Body)
+		fmt.Printf("%d\n", res.StatusCode)
+		io.Copy(os.Stdout, res.Body)
 		res.Body.Close()
-		if err != nil {
-			fmt.Fprint(os.Stderr, err)
-			os.Exit(1)
-		}
-		fmt.Printf("%s\n", body)
 	}
 }
