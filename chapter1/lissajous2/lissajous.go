1751b040ecc9d9b80d689b525169e111365fe082
ariohe <70228762+zoooooway@users.noreply.github.com>
2022-11-10 17:55:08 +0800

    enhance lissajous

---
+++ chapter1/lissajous2/lissajous.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+var palette = []color.Color{color.RGBA{0x02, 0xbc, 0xbb, 0xff}, color.RGBA{0x98, 0x07, 0x23, 0xff}}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+)
+
+func main() {
+	http.HandleFunc("/lissajous", func(w http.ResponseWriter, r *http.Request) {
+		rand.Seed(time.Now().UTC().UnixNano())
+		cyc, err := strconv.Atoi(r.FormValue("cyc"))
+		if err != nil {
+			// 不指定content-type则响应会使用application/octet-stream形式，变成下载文件
+			w.Header().Add("content-type", "text/html")
+			fmt.Fprintf(w, "bad parameter cyc")
+		}
+		lissajous(w, cyc)
+	})
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+
+}
+
+func lissajous(out io.Writer, cycles int) {
+
+	const (
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < float64(cycles*2)*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
